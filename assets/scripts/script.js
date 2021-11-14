c6b09c61516bbe567979f475218c38c3199a2295
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-14 00:02:14 -0500

    Refactored code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,8 @@
-// Variables for loading HTML elements
-var elemStartBtn = document.querySelector("#start_quiz"); // The 'Start quiz' button
+// Variables for HTML elements
 var elemSecs = document.querySelectorAll("section");
-var elemSecOpening = document.querySelector("#opening");
 var elemSecCurrentQ;
 var elemAnswerDisp = document.querySelector("#answer_display");
-var elemScoreDisp = document.querySelector("#score");
-var elemSubmitBtn = document.querySelector("#score_submit");
-var elemRestartBtn = document.querySelector("#restart");
 var elemHighScores = document.querySelector("#highscores_list");
-var elemViewHighScores = document.querySelector("#view_highscores");
-var elemClearHighScore = document.querySelector("#clear_scores");
 
 // Counter to show what question is being answered
 var questionCounter = 1;
@@ -22,10 +15,14 @@ var yourScore = {
 }
 
 // Set initial value of countdown timer
-var timeLeft = 20;
+var initialCountdownValue = 30;
+// Used to display the actual time left for the player
+var timeLeft = initialCountdownValue;
+
+///////////////////////////////// FUNCTIONS /////////////////////////////////
 
 // Show a selected <section> element and hide the other <section> elements
-var showSecs = function (show) { // Parameter accepts an element selector
+var showSection = function (show) { // Parameter accepts an element selector
     for (var i = 0; i < elemSecs.length; i++) {
         elemSecs[i].setAttribute("data-visibility","hidden");
     }
@@ -35,7 +32,7 @@ var showSecs = function (show) { // Parameter accepts an element selector
 // Check whether the player selected the correct answer
 var checkAnswer = function(event) {
     var btnClass = event.target.getAttribute("class");
-    if (btnClass === "true") {
+    if (btnClass === "true") { // Buttons for the correct answer have a CSS class called "true"
         elemAnswerDisp.textContent = "Correct";
         setTimeout (function(){
             elemAnswerDisp.textContent = "";
@@ -47,42 +44,37 @@ var checkAnswer = function(event) {
     }
 }
 
-// Display each quiz question
-var showQuestions = function() {
+// Display the quiz questions
+var showQuestionPages = function() {
 
-    var elemSecCurrentQName;
-    
-    if (questionCounter === 1) {
-        elemSecCurrentQName = "section[data-question='" + questionCounter + "']";
-        showSecs(elemSecCurrentQName);
+    // Show the appropriate quiz question/page
+    var showQuestion = function(question) {
+        var elemSecCurrentQName = "section[data-question='" + question + "']";
+        showSection(elemSecCurrentQName);
         document.querySelector(elemSecCurrentQName).addEventListener('click', checkAnswer);
-    } else if (questionCounter === 2) {
-        elemSecCurrentQName = "section[data-question='" + questionCounter + "']";
-        showSecs(elemSecCurrentQName);
-        document.querySelector(elemSecCurrentQName).addEventListener('click', checkAnswer);
-    } else if (questionCounter === 3) {
-        elemSecCurrentQName = "section[data-question='" + questionCounter + "']";
-        showSecs(elemSecCurrentQName);
-        document.querySelector(elemSecCurrentQName).addEventListener('click', checkAnswer);
+    }
+    if (questionCounter === 1) { // For question1 
+        showQuestion(1);
+    } else if (questionCounter === 2) { // For question2
+        showQuestion(2);
+    } else if (questionCounter === 3) { // and so on...
+        showQuestion(3);
     } else if (questionCounter === 4) {
-        elemSecCurrentQName = "section[data-question='" + questionCounter + "']";
-        showSecs(elemSecCurrentQName);
-        document.querySelector(elemSecCurrentQName).addEventListener('click', checkAnswer);
+        showQuestion(4);
     } else if (questionCounter === 5) {
-        elemSecCurrentQName = "section[data-question='" + questionCounter + "']";
-        showSecs(elemSecCurrentQName);
-        document.querySelector(elemSecCurrentQName).addEventListener('click', checkAnswer);
-    } else {
+        showQuestion(5);
+    } else { // If all questions have been answered
         endGame();
     }
 
 }
 
-// Display scores
+// Display high scores and reset the quiz if needed
 var displayScores = function() {
 
     elemHighScores.innerHTML = "";
 
+    // Display the high scores that were extracted from localStorage
     if (highScores !== null) {
         for (var i=0; i < highScores.length; i++) {
             var elemHighScoresItem = document.createElement("li");
@@ -93,10 +85,10 @@ var displayScores = function() {
         }
     }
 
-    // Reset the quiz
-    elemRestartBtn.addEventListener("click", function() {
-        showSecs("#opening");
-        timeLeft = 20;
+    // Provide ability to reset the quiz
+    document.querySelector("#restart").addEventListener("click", function() {
+        showSection("#opening");
+        timeLeft = initialCountdownValue;
         questionCounter = 1;
     })
 }
@@ -106,15 +98,14 @@ var viewHighScores = function() {
     clearInterval(runCountdown);
     getHighScore();
     displayScores();
-    showSecs("#highscores");
+    showSection("#highscores");
 }
 
 // Run the game until countdown timer hits zero
 var runCountdown;
 var runGame = function() {
 
-    elemSecOpening.setAttribute("class", "hidden");
-
+    //Start running the countdown timer
     runCountdown = setInterval(function() {
         --timeLeft;
         var timeLeftDisplay = document.querySelector("#time_left");
@@ -124,7 +115,7 @@ var runGame = function() {
         if (timeLeft <= 0) {
             endGame();
         } else {
-            showQuestions();
+            showQuestionPages();
         }
     }, 1000);
 }
@@ -143,15 +134,15 @@ var recordScore = function (event) {
     yourScore.score = timeLeft;
     getHighScore();
     
+    // Write high scores to localStorage
     if (highScores === null) {
        highScores = [];
     }
     highScores.push(yourScore);
     localStorage.setItem("high_scores", JSON.stringify(highScores));
 
-    showSecs("#highscores");
+    showSection("#highscores");
     document.querySelector("#name").value = "";
-
     displayScores();
 }
 
@@ -159,23 +150,23 @@ var recordScore = function (event) {
 var endGame = function () {
     clearInterval(runCountdown);
 
-    // Hide all <Sec>, except for the one that displays the current question
-    showSecs("#endgame");
+    // Hide all <section>, except for the one that displays the current question
+    showSection("#endgame");
 
-    elemScoreDisp.textContent = timeLeft;
-
-    elemSubmitBtn.addEventListener("click",recordScore);
-
-    highScores = [];
+    document.querySelector("#score").textContent = timeLeft;
+    document.querySelector("#score_submit").addEventListener("click",recordScore);
+    highScores = []; // It seems that failing to have this causes high score displays to duplicate
 
 }
 
-// Clear High Scores List
+// Clear the 'High Scores' List
 var clearHighScores = function(){
     localStorage.removeItem("high_scores");
     elemHighScores.innerHTML = "";
 }
 
-elemStartBtn.addEventListener('click', runGame);
-elemViewHighScores.addEventListener('click', viewHighScores);
-elemClearHighScore.addEventListener('click',clearHighScores);
+///////////////////////////////// EXECUTION STARTS HERE /////////////////////////////////
+
+document.querySelector("#start_quiz").addEventListener('click', runGame);
+document.querySelector("#view_highscores").addEventListener('click', viewHighScores);
+document.querySelector("#clear_scores").addEventListener('click', clearHighScores);
